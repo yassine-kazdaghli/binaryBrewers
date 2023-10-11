8aeb90684d2557caf872d2a9c42bd23490151c6b
psychicseizures <anichema@gmail.com>
2023-10-11 09:46:41 +0200

    interactive background Mads

--- my-react-app/client/src/components/InterActiveBackground/interActiveBackground.jsx
+++ my-react-app/client/src/components/InterActiveBackground/interActiveBackground.jsx
@@ -1,13 +1,15 @@
 import Particles from "react-tsparticles";
-//import { loadSlim } from "tsparticles-slim"; // loads tsparticles-slim
-import { loadFull } from "tsparticles"; // loads tsparticles
+import { loadSlim } from "tsparticles-slim"; // loads tsparticles-slim
+//import { loadFull } from "tsparticles"; // loads tsparticles
 import { useCallback, useMemo } from "react";
 
-
-const InteractiveBackground = (props) => {
-  
+// tsParticles Repository: https://github.com/matteobruni/tsparticles
+// tsParticles Website: https://particles.js.org/
+const InteractiveBackground  = (props) => {
+  // using useMemo is not mandatory, but it's recommended since this value can be memoized if static
   const options = useMemo(() => {
-
+    // using an empty options object will load the default options, which are static particles with no background and 3px radius, opacity 100%, white color
+    // all options can be found here: https://particles.js.org/docs/interfaces/Options_Interfaces_IOptions.IOptions.html
     return {
       background: {
         color: "#000", // this sets a background color for the canvas
@@ -57,11 +59,11 @@ const InteractiveBackground = (props) => {
 
   // useCallback is not mandatory, but it's recommended since this callback can be memoized if static
   const particlesInit = useCallback((engine) => {
-    //loadSlim(engine);
-    loadFull(engine); // for this sample the slim version is enough, choose whatever you prefer, slim is smaller in size but doesn't have all the plugins and the mouse trail feature
+    loadSlim(engine);
+    // loadFull(engine); // for this sample the slim version is enough, choose whatever you prefer, slim is smaller in size but doesn't have all the plugins and the mouse trail feature
   }, []);
 
-  //setting an id can be useful for identifying the right particles component, this is useful for multiple instances or reusable components
+  // setting an id can be useful for identifying the right particles component, this is useful for multiple instances or reusable components
   return <Particles id={props.id} init={particlesInit} options={options} />;
 };
 
