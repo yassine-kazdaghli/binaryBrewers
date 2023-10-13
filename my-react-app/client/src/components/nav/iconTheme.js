b8042f856b252699cd773feec5d1bbd30c18788b
yassine-kazdaghli <kazdaghli.yassine@gmail.com>
2023-10-13 11:17:43 +0200

    nav animations

--- my-react-app/client/src/components/nav/iconTheme.js
+++ my-react-app/client/src/components/nav/iconTheme.js
@@ -8,13 +8,15 @@ const theme = createTheme({
         root: {
           '&:hover': {
             backgroundColor: '#009b5c',
+            transform: 'rotate(3deg)'
             
           },
           '&:active': {
             background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90% )', // Gradient background on click
             backgroundSize: '200% 200%',
             transition: 'background 0.8s',
-            scale: '1.1'
+            scale: '1.1',
+            transform: 'rotate(-3deg)'
           },
         },
       },
