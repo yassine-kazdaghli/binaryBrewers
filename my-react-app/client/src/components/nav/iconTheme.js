47a88ce31c7ffd2809e03acb7683293f3f46df2f
psychicseizures <anichema@gmail.com>
2023-10-17 13:19:41 +0200

    about fonts digital fonts

--- my-react-app/client/src/components/nav/iconTheme.js
+++ my-react-app/client/src/components/nav/iconTheme.js
@@ -12,8 +12,15 @@ const theme = createTheme({
           backdropFilter:'blur (1px)',
           borderBottom:'1px solid black)',
           height:'max-content',
-          marginTop:"0"
-          
+          marginTop:"0",
+          fonts: {
+            primary: 'Press Start 2P, monospace', // Replace 'Press Start 2P' with the desired Google Font
+          },
+          // Other theme properties can be defined here
+          globalStyles: `
+            @import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
+            /* You can add other global styles here if needed */
+          `,
           
           
         },
