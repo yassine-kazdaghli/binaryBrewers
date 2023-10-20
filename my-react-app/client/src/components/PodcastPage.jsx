b9c47aa0fa971929d35c56ead50246ec6948ca48
yassine-kazdaghli <kazdaghli.yassine@gmail.com>
2023-10-20 18:14:52 +0200

    now the podcast page is done

--- my-react-app/client/src/components/PodcastPage.jsx
+++ my-react-app/client/src/components/PodcastPage.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import './podcastPage.css'
 import CardExampleCard from './podcastCard/podcastCard';
 
 function PodcastPage() {
-  return <div>
+  return <div className='podcasts'>
+   <CardExampleCard/>
     <CardExampleCard/>
-    
+    <CardExampleCard/>
+    <CardExampleCard/>
+    <CardExampleCard/>
+    <CardExampleCard/>    
   </div>;
 }
 
--- my-react-app/client/src/components/podcastCard/podcastCard.jsx
+++ my-react-app/client/src/components/podcastCard/podcastCard.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-
-import { Box, Card, CardContent, CardMedia, IconButton, Typography } from '@mui/material';
+import { Box, Card, CardContent, IconButton, Typography } from '@mui/material';
 import { SkipPrevious as SkipPreviousIcon, PlayArrow as PlayArrowIcon, SkipNext as SkipNextIcon } from '@mui/icons-material';
-import podcast from './podcast.jpg';
 import { useMusicPlayer } from '../mediaPlayer/musicPlayerContext';
 import PauseRounded from '@mui/icons-material/PauseRounded';
 
@@ -13,55 +11,47 @@ const CardExampleCard = () => {
     <Card 
       sx={{ 
         display: 'flex', 
+        flexDirection: 'column', // Changed to column for better organization
         width: '80%', 
-        height: '30vh', 
-        flexDirection:'row',
+        height: 'auto', // Use auto to ensure content fits
         backgroundColor: 'transparent', 
         color: 'whitesmoke', 
         border: '1px solid #333', 
         borderRadius: '15px',
-        overflow: 'hidden',
-        justifyContent:'center',
-        alignItems:'center',
+        overflow: 'visible',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '1rem',
+        marginBottom: '1rem'
       }}
     >
+      <CardContent sx={{ textAlign: 'center' }}>
+        <Typography component="div" variant="h5">
+          {songs[currentIndex]?.title}
+        </Typography>
+        <Typography variant="subtitle1" color="white" component="div">
+          {songs[currentIndex]?.artist}
+        </Typography>
+      </CardContent>
       <Box 
         sx={{ 
           display: 'flex', 
-          flexDirection: 'row', 
-          gap: '2rem',
-          width: '75%',
+          flexDirection: 'row',
+          gap: '1rem',
           alignItems: 'center',
-          justifyContent: 'space-between',
-          padding: '2rem'
+          justifyContent: 'center',
         }}
       >
-        <CardContent sx={{ flex: '1 0 auto' }}>
-          <Typography component="div" variant="h4">
-            {songs[currentIndex]?.title}
-          </Typography>
-          <Typography variant="subtitle1" color="white" component="div">
-            {songs[currentIndex]?.artist}
-          </Typography>
-        </CardContent>
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          <IconButton aria-label="previous" onClick={playPrevious}>
-            <SkipPreviousIcon />
-          </IconButton>
-          <IconButton aria-label="play/pause" onClick={handlePlayPause}>
-            {isPlaying ?  <PauseRounded sx={{ height: 38, width: 38 }}  /> : <PlayArrowIcon sx={{ height: 38, width: 38 }} />}
-          </IconButton>
-          <IconButton aria-label="next" onClick={playNext}>
-            <SkipNextIcon />
-          </IconButton>
-        </Box>
+        <IconButton aria-label="previous" onClick={playPrevious}>
+          <SkipPreviousIcon />
+        </IconButton>
+        <IconButton aria-label="play/pause" onClick={handlePlayPause}>
+          {isPlaying ?  <PauseRounded sx={{ height: 38, width: 38 }}  /> : <PlayArrowIcon sx={{ height: 38, width: 38 }} />}
+        </IconButton>
+        <IconButton aria-label="next" onClick={playNext}>
+          <SkipNextIcon />
+        </IconButton>
       </Box>
-      <CardMedia
-        component="img"
-        sx={{ width: '30%', objectFit: 'center' }}
-        src={podcast}
-        alt="Live from space album cover"
-      />
     </Card>
   );
 }
