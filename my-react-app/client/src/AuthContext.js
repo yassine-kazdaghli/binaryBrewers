b968cd23b6aaf33daab1d6a609e2ee2a75367cf1
yassine-kazdaghli <kazdaghli.yassine@gmail.com>
2024-01-18 22:47:45 +0100

    deploying back end

--- my-react-app/client/src/AuthContext.js
+++ my-react-app/client/src/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import axios from 'axios';
+
 import Cookies from 'js-cookie';
 import api from './services/api.js';
 
